04eaff452d3f646f77696f25d022a60043ddf5ce
maxine <maxbeckie@tutanota.com>
2022-12-21 15:16:28 -0800

    a few ideas
    	- not much yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 
-struct SymmetricGroup(order: u64) {
+struct SymmetricGroup {
     todo!();
 }
 
-struct AlternatingGroup(order: u64) {
+struct AlternatingGroup {
     todo!();
 }
 
+
+//maybe define in a categorical way, with diagrams, etc, as well as a set way. 
 trait Group {
     todo!();
 }
 
+//was thinking of making morphisms just functions O -> O, but they need extra structure :<
+struct Category<O, M> {
+    objects: O,
+    morphisms: M,
+}
+
+//just a vec with order forgotten and some methods .. is this a Good IDea?? who knows.. 
+//Also, make a poset sometime. 
+struct Set<E> {
+    elements: Vec<E>,
+}
+
+    
+    
 

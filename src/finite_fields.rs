722256c00b6b7b96e7277b21c91c4eca5a6c8c04
maxine <maxbeckie@tutanota.com>
2023-01-06 10:28:03 -0800

    some more overloading
    	- implemented Sub and Neg for IntegerModN
    
    		- tests pass
    
    	- added Prime wrapper struct, implemented Add for it (and
    	  derived stuff)

--- src/finite_fields.rs
+++ src/finite_fields.rs
@@ -1,9 +1,26 @@
-use core::ops::Add;
+use std::ops::{Add, Neg, Sub};
+
+///wrapper around u64 for primes
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Prime {
+    val: u64,
+}
+
+impl Add for Prime {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            val: self.val + rhs.val,
+        }
+    }
+}
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 struct IntegerModN {
-    val: usize, // a representative for the class of val mod n
-    n: usize,   // the order of the group
+    val: u64, // a representative for the class of val mod n
+    n: u64,   // the order of the group
+              // NOTE: val < n always. important.
 }
 
 impl Add for IntegerModN {
@@ -16,6 +33,34 @@ impl Add for IntegerModN {
                 true => self.n,
                 false => panic!(),
             },
+        }
+    }
+}
+
+impl Neg for IntegerModN {
+    type Output = Self;
+
+    fn neg(self) -> Self {
+        if self.val == 0 {
+            self
+        } else {
+            Self {
+                val: self.n - self.val,
+                n: self.n,
+            }
+        }
+    }
+}
+
+impl Sub for IntegerModN {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        match self.n == rhs.n {
+            true => Self { val: (self.val + (-rhs).val) % self.n,
+                            n: self.n,
+            },
+            false => {panic!();},
         }
     }
 }
@@ -44,5 +89,40 @@ mod test {
         let n7 = IntegerModN { val: 1, n: 7 };
         let n3 = IntegerModN { val: 1, n: 3 };
         let n_invalid = n7 + n3;
+    }
+
+    #[test]
+    fn neg() {
+        let num = IntegerModN { val: 1, n: 7 };
+        let minus_num = IntegerModN { val: 6, n: 7 };
+        assert_eq!(minus_num, -num);
+        let zero = IntegerModN { val: 0, n: 234 };
+        assert_eq!(zero, -zero);
+    }
+
+    #[test]
+    fn sub() {
+        //two nonzero, no wrap
+        let num1 = IntegerModN { val: 5, n: 9 };
+        let num2 = IntegerModN { val: 3, n: 9 };
+        let expected1 = IntegerModN { val: 2, n: 9 };
+        assert_eq!(num1 - num2, expected1);
+
+        //two nonzero, wrap
+        let num3 = IntegerModN { val: 6, n: 9 };
+        let expected2 = IntegerModN { val: 8, n: 9 };
+        assert_eq!(num1 - num3, expected2);
+
+        //one zero
+        let zero = IntegerModN { val: 0, n: 9 };
+        assert_eq!(num1 - zero, num1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn sub_invalid() {
+        let num1 = IntegerModN { val: 1, n: 2 };
+        let num2 = IntegerModN { val: 1, n: 3 };
+        let num3 = num1 - num2;
     }
 }

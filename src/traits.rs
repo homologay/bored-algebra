87039e4189228fc513296673329617b733acf389
maxine <maxbeckie@tutanota.com>
2023-01-08 21:10:03 -0800

    the ideas from lib.rs have a new file, basically
    	- bottom text

---
+++ src/traits.rs
@@ -0,0 +1,7 @@
+//! here lie traits for abstractions like Group, Ring, ...
+
+// Group should extend Add, Neg, Sub
+
+// Ring should extend Group, Mul
+
+// not really sure how to do this, maybe should read up my books on traits

cd402e91f0e20b4463fa9dc2157d3ae0177d0c3a
maxine <maxbeckie@tutanota.com>
2023-02-17 03:02:51 -0800

    wrote some thoughts for local, sub
      -tired

--- src/sub.rs
+++ src/sub.rs
@@ -1,1 +1,15 @@
 //! Submodules
+
+use crate::module::{ModType, RingType};
+
+/// An $R$-submodule of $M\in \text{Mod}(R)$.
+pub trait Sub<R: RingType, M: ModType<R>>: ModType<R> {
+    todo!();
+
+    // it's possible a more general implementation could extend Into<ModType<R>> instead of ModType<R>,
+    // to account for situations where mathematicians say things like "consider the image of this
+    // homomorphism as a submodule of blah ...."
+
+    // It's also possible this could be too permissive. Instead, could make ways to consider
+    // the image of a homomorphism as a submodule or things like that ..... idk
+}
